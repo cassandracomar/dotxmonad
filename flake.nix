e4d3d39bc735c55504fb1ac43fa39a68a3eb99cc
Cassandra Comar <cassandra@ditto.live>
2022-02-21 00:24:50 -0500

    update for fixed nix build

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "xmonad personal configuration";
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  #inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
@@ -12,14 +13,10 @@
             # This overlay adds our project to pkgs
             xmonad-personal = final.haskell-nix.project' {
               src = ./.;
-              compiler-nix-name = "ghc921";
+              compiler-nix-name = "ghc8107";
               # This is used by `nix develop .` to open a shell for use with
               # `cabal`, `hlint` and `haskell-language-server`
-              shell.tools = {
-                cabal = { };
-                hlint = { };
-                haskell-language-server = { };
-              };
+              shell.tools = { cabal = { }; };
               # Non-Haskell shell tools go here
               shell.buildInputs = with pkgs; [ nixpkgs-fmt ];
             };
@@ -31,6 +28,7 @@
         };
         flake = pkgs.xmonad-personal.flake { };
       in flake // {
+        packages = flake.packages;
         # Built by `nix build .`
         defaultPackage = flake.packages."xmonad-personal:exe:xmonad";
       });
